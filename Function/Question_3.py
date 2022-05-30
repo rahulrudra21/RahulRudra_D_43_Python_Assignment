0a1cd9104e7ee158d8f6ed6b16964ff9fc7a05ca
rahulrudra21 <106325909+rahulrudra21@users.noreply.github.com>
2022-05-30 14:35:38 +0530

    Create Question_3.py

---
+++ Function/Question_3.py
@@ -0,0 +1,16 @@
+'''remove duplicate in list'''
+def info(lst):
+    a = []
+
+    for i in lst:
+        if i not in a:
+            a.append(i)
+
+    return a
+
+
+n = int(input())
+lst = []
+for i in range(1, n + 1):
+    lst.append(int(input()))
+print(info(lst))
