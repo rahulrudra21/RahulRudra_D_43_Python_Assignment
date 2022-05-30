fab3ae07a34db5cacaed9b98301dccaf3a710acc
rahulrudra21 <106325909+rahulrudra21@users.noreply.github.com>
2022-05-30 15:12:14 +0530

    Create indexing in array.py

---
+++ Numpy/indexing in array.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([[-1, 2, 0, 4],
+                [4, -0.5, 6, 0],
+                [2.6, 0, 7, 8],
+                [3, -7, 4, 2.0]])
+print("Initial Array: ")
+print(arr)
